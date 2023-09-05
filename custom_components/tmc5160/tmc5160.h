7926d1b50a7c9db1053cae7fa199c0f4d091ee6d
Niall Langley <niall.langley@gmail.com>
2023-09-05 12:26:37 +0100

    Added config for reset pin

--- custom_components/tmc5160/tmc5160.h
+++ custom_components/tmc5160/tmc5160.h
@@ -7,10 +7,13 @@
 namespace esphome {
 namespace tmc5160 {
 
+// Can't be called TMC5160, as that's the name of the library we use to 
+// talk to the driver using SPI
 class TMC5160_Stepper : public stepper::Stepper, public Component {
  public:
   void set_cs_pin(InternalGPIOPin *cs_pin) { cs_pin_ = cs_pin; }
   void set_sleep_pin(GPIOPin *sleep_pin) { this->sleep_pin_ = sleep_pin; }
+  void set_reset_pin(GPIOPin *reset_pin) { this->reset_pin_ = reset_pin; }
   void setup() override;
   void dump_config() override;
   void loop() override;
@@ -20,6 +23,7 @@ class TMC5160_Stepper : public stepper::Stepper, public Component {
  protected:
   InternalGPIOPin *cs_pin_;
   GPIOPin *sleep_pin_{nullptr};
+  GPIOPin *reset_pin_{nullptr};
   bool sleep_pin_state_;
 };
 
