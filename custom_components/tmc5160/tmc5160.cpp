fd1ffacc0f61bf15d2927abbdccbed4ab525c88d
Niall Langley <niall.langley@gmail.com>
2023-09-08 20:05:30 +0100

    Added yaml config for driver current settings, and refactored driver reset into a function

--- custom_components/tmc5160/tmc5160.cpp
+++ custom_components/tmc5160/tmc5160.cpp
@@ -18,33 +18,28 @@ void TMC5160_Stepper::setup() {
   // VCC_IO can be dropped low. For drivers with VCC_IO supporting 3.3-5V, the logic is driven
   // by the onboard step-down, so VCC_IO has a very low current draw and we can power it from a
   // data pin, which means we can drop it low briefly to reset the driver before setup.
-  if (this->reset_pin_ != nullptr) {
-    ESP_LOGCONFIG(TAG, "Resetting TMC5160...");
-    this->reset_pin_->setup();
-    this->reset_pin_->digital_write(false); // Set to low to reset the TMC5160
-    delay(100);
-    this->reset_pin_->digital_write(true); // Set to high to power the TMC5160
-  }
+  this->reset_driver();
 
   ESP_LOGCONFIG(TAG, "Setting up TMC5160...");
   if (this->sleep_pin_ != nullptr) {
     this->sleep_pin_->setup();
-    this->sleep_pin_->digital_write(false);
-    this->sleep_pin_state_ = false;
   }
+  this->enable_driver(false);
 
   this->cs_pin_->setup();
   this->cs_pin_->digital_write(false);
-
   uint8_t cs_pin = this->cs_pin_->get_pin();
   motor = new TMC5160_SPI(cs_pin); //Use default SPI peripheral and SPI settings.
 
   // This sets the motor & driver parameters /!\ run the configWizard for your driver and motor for fine tuning !
   TMC5160::PowerStageParameters powerStageParams; // defaults.
   TMC5160::MotorParameters motorParams;
-  motorParams.globalScaler = 98; // Adapt to your driver and motor (check TMC5160 datasheet - "Selecting sense resistors")
-  motorParams.irun = 31;
-  motorParams.ihold = 16;
+  // TODO: These should be mandatory settings on the YAML
+
+  float maxRmsCurrent = 0.33 / (this->current_resistor_ * sqrt(2.0));
+  motorParams.globalScaler = constrain(floor(this->motor_current_ * 256.0 / maxRmsCurrent), 32, 256);
+  motorParams.irun = constrain(floor(this->motor_current_ * 31.0 / (maxRmsCurrent * (float)motorParams.globalScaler / 256.0)), 0, 31);
+  motorParams.ihold = constrain(floor((float)motorParams.irun * this->motor_hold_power_), 0, 31);
 
   SPI.begin();
   motor->begin(powerStageParams, motorParams, TMC5160::NORMAL_MOTOR_DIRECTION);
@@ -63,10 +58,40 @@ void TMC5160_Stepper::dump_config() {
   ESP_LOGCONFIG(TAG, "TMC5160:");
   LOG_PIN("  SPI Chip Select Pin: ", this->cs_pin_);
   LOG_PIN("  Sleep Pin: ", this->sleep_pin_);
-  
+  LOG_PIN("  Reset Pin: ", this->reset_pin_);
+  ESP_LOGCONFIG(TAG, "  Current Resistor:  %f", this->current_resistor_);
+  ESP_LOGCONFIG(TAG, "  Motor Current:  %f", this->motor_current_);
+  ESP_LOGCONFIG(TAG, "  Motor Hold Power:  %f", this->motor_hold_power_);
+
+  ESP_LOGCONFIG(TAG, "  Motor Driver Info:  %d", (int)motor->readStatus());
+  ESP_LOGCONFIG(TAG, "    Status: %d", (int)motor->readStatus());
+  ESP_LOGCONFIG(TAG, "    Status: %d", (int)motor->readStatus());
+  ESP_LOGCONFIG(TAG, "    Status: %d", (int)motor->readStatus());
+  ESP_LOGCONFIG(TAG, "    Status: %d", (int)motor->readStatus());
+
   
   LOG_STEPPER(this);
 }
+
+void TMC5160_Stepper::enable_driver(bool state)
+{
+  if (this->sleep_pin_ != nullptr) {
+    this->sleep_pin_->digital_write(!state); // Active low
+  }
+  this->is_driver_enabled_ = state;
+}
+
+void TMC5160_Stepper::reset_driver()
+{
+  if (this->reset_pin_ != nullptr) {
+    ESP_LOGCONFIG(TAG, "Resetting TMC5160...");
+    this->reset_pin_->setup();
+    this->reset_pin_->digital_write(false); // Set to low to reset the TMC5160
+    delay(100);
+    this->reset_pin_->digital_write(true); // Set to high to power the TMC5160
+  }
+}
+
 
 // Set initial position, or reset position if needed
 void TMC5160_Stepper::reset_position(float position) {
@@ -78,45 +103,63 @@ void TMC5160_Stepper::reset_position(float position) {
 void TMC5160_Stepper::loop() {
   // bool at_target = this->has_reached_target();
 
+  // We should be able to talk to the driver even if it is not enabled, but
+  // we won't get movement until we enable it.
+  this->enable_driver(true);
 
-  if (this->sleep_pin_ != nullptr) {
-    this->sleep_pin_->setup();
-    this->sleep_pin_->digital_write(false);
-    this->sleep_pin_state_ = false;
-  }
+  // If the sleep pin state is true, the driver is active
 
-  uint32_t now = millis();
-  static unsigned long t_dirchange, t_echo;
-  static bool dir;
+  // is_motor_moving = motor->getCurrentSpeed() != 0;
 
-  // every n seconds or so...
-  if ( now - t_dirchange > 10000 )
-  {
-    t_dirchange = now;
+  // Check the speed, does it go negative in reverse? If so
+  // we can use this to determine if we are moving the correct
+  // direction
 
-    // reverse direction
-    dir = !dir;
-    motor->setTargetPosition(dir ? 2000 : 0);  // 1 full rotation = 200s/rev
-  }
 
-  // print out current position
-  if( now - t_echo > 100 )
-  {
-    t_echo = now;
 
-    // get the current target position
-    float xactual = motor->getCurrentPosition();
-    float vactual = motor->getCurrentSpeed();
+
+
+
+  // uint32_t now = millis();
+  // static unsigned long t_dirchange, t_echo;
+  // static bool dir;
+
+  // // every n seconds or so...
+  // if ( now - t_dirchange > 30000 )
+  // {
+  //   t_dirchange = now;
+
+  //   // reverse direction
+  //   dir = !dir;
+  //   motor->setTargetPosition(dir ? 2000 : 0);  // 1 full rotation = 200s/rev
+  // }
+
+  // // print out current position
+  // if( now - t_echo > 100 )
+  // {
+  //   t_echo = now;
+
+  //   // get the current target position
+  //   float xactual = motor->getCurrentPosition();
+  //   float vactual = motor->getCurrentSpeed();
     
-    ESP_LOGI(TAG, "Motor status:  %d", (int)motor->readStatus());
-    ESP_LOGD(TAG, "Current position:  %f", xactual);
-    ESP_LOGD(TAG, "Current speed:     %f", vactual);
-  }
+  //   ESP_LOGI(TAG, "Motor status:  %d", (int)motor->readStatus());
+  //   ESP_LOGD(TAG, "Current position:  %f", xactual);
+  //   ESP_LOGD(TAG, "Current speed:     %f", vactual);
+  // }
+
+
+
+
+
+
+
+
 
   // if (this->sleep_pin_ != nullptr) {
-  //   bool sleep_rising_edge = !sleep_pin_state_ & !at_target;
+  //   bool sleep_rising_edge = !is_driver_enabled_ & !at_target;
   //   this->sleep_pin_->digital_write(!at_target);
-  //   this->sleep_pin_state_ = !at_target;
+  //   this->is_driver_enabled_ = !at_target;
   //   if (sleep_rising_edge) {
   //     delayMicroseconds(1000);
   //   }
